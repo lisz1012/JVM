b23332d34d672e5acecb933b031215a2393e2cff
lisz1012 <lisz1012@163.com>
2020-09-30 22:55:51 -0700

    Add notes

---
+++ src/com/mashibing/jvm/c3_jmm/T05_Synchronized.java
@@ -0,0 +1,11 @@
+package com.mashibing.jvm.c3_jmm;
+
+public class T05_Synchronized {
+	void m() {
+		synchronized (this) {}
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
--- src/com/mashibing/jvm/c3_jmm/TestVolatile.java
+++ src/com/mashibing/jvm/c3_jmm/TestVolatile.java
@@ -3,4 +3,8 @@ package com.mashibing.jvm.c3_jmm;
 public class TestVolatile {
     int i;
     volatile int j;
+
+    public static void main(String[] args) {
+
+    }
 }
