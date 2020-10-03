a60343ac193e9e322a0bc5503f9f11ca9c220253
lisz1012 <lisz1012@163.com>
2020-10-02 21:33:51 -0700

    Add notes

--- src/com/mashibing/jvm/c4_RuntimeDataAreaAndInstructionSet/Hello_01.java
+++ src/com/mashibing/jvm/c4_RuntimeDataAreaAndInstructionSet/Hello_01.java
@@ -7,15 +7,15 @@ public class Hello_01 {
 
     public void m1() {
         int i = 200;
-    }
+    }  // sipush，short入栈，因为大于127了
 
     public void m2(int k) {
         int i = 300;
-    }
+    }  // istore_2了，因为非静态方法多了个this
 
     public void add(int a, int b) {
         int c = a + b;
-    }
+    }  // 局部变量表0：this，1：a，2：b，3：c
 
     public void m3() {
         Object o = null;
